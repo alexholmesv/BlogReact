2d890710210887a3ee7209c957cd7f23558a1693
Alex Holmes <alexander.holmes@gmail.com>
2017-10-17 20:53:10 -0300

    new port in index

--- index.js
+++ index.js
@@ -93,6 +93,6 @@ app.post('/login', function (req, res) {
     })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
